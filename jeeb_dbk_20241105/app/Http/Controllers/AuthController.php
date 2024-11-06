eb632708c0cf43852c907cb6bdde00fa3fbd034b
unknown <UFG@REDES-20.ufg.local>
2024-11-05 19:56:04 -0600

    version1.1
    actualizacion

---
+++ jeeb_dbk_20241105/app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request): void{
+        $validatedData = $request->validate(rules : [
+            'name'=>['required','string','max:255'],
+            'email'=>['required','string','email','max:255','unique:users'],
+            'password'=>['required','string','min:8','max:20'],
+        ] );
+
+        $user = User::create(attributes: [
+            'name' => $validadtedData['name'],
+            'email' => $validadtedData['email'],
+            'password' => Hash::make(value: $validatedData['password']),
+        ]);
+
+        &token = $user ->createToken('auth_token')->plainTextToken;
+
+        return response() ->json(data: [
+            "success" => true,
+            "errors" => [
+                "code" =>0,
+                "msg" =>""
+            ],
+            "data" => [
+                "access_token" =>$token,
+                "token-type" => "Bearer"
+            ],
+            "msg"=>"Usuario creado satisfactoriamente",
+            "count" =>1
+        ]);
+    }
+}
