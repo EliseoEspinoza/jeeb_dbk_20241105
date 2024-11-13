31d0d810aa5ad22f8a286c76d60b52c0d2d5715a
unknown <UFG@REDES-22.ufg.local>
2024-11-12 20:10:25 -0600

    version 1.2
    hola

--- jeeb_dbk_20241105/app/Http/Controllers/AuthController.php
+++ jeeb_dbk_20241105/app/Http/Controllers/AuthController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+
 
 class AuthController extends Controller
 {
     //
-    public function register(Request $request): void{
-        $validatedData = $request->validate(rules : [
+    public function register(Request $request)  {
+        $validatedData = $request->validate([
             'name'=>['required','string','max:255'],
             'email'=>['required','string','email','max:255','unique:users'],
             'password'=>['required','string','min:8','max:20'],
@@ -35,5 +37,46 @@ class AuthController extends Controller
             "msg"=>"Usuario creado satisfactoriamente",
             "count" =>1
         ]);
+    }
+
+    public funtion login (Request $request) {
+        if(!Auth::attempt($request->only("email","password"))){
+            return response()->json([
+                "success"=> false,
+                "errors"=> [
+                    "code" =>401,
+                ],
+                "data"=> "",
+                "count"=> 0
+            ], 401);
+        }
+        $user = User::where("email", $request->email)->firstOrFail();
+        $token = $user -> createToken("auth_token")->plainTextToken;
+
+        return response() ->json(data: [
+            "success"=> true,
+            "errors"=> [
+                "code"=> 200,
+                "msg"=> ""
+                ],
+                "data"=> [
+                    "acess token"=> $token,
+                    "token_type"=> "Bearer"
+                "msg"=>"Ha iniciado secion correctamente",
+                "count"=> 1
+        ],200);
+    }
+
+
+    public function me(Request $request) {
+        return response()->json( [
+            "success"=> true,
+            "errors"=> [
+                "code"=> 200,
+                "msg"=>""
+                ],
+                "data"=>$request->user(),
+                "count"=> 1
+        ], 200);
     }
 }
--- jeeb_dbk_20241105/routes/api.php
+++ jeeb_dbk_20241105/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/nuevousuario', [AuthController::class,"register"]);
+Route::post("/login", [AuthController::class,"login"]);
+Route::post("/usuario", [AuthController::class,"me"])->middleware( 'auth:santum');
